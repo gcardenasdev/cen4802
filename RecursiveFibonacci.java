06bd39bdad2ced691082667b26bed91ffd6b681e
Gabriela Cardenas <gcardenasdev@gmail.com>
2024-01-21 21:58:42 -0500

    Uploaded class file with javadoc
    Updated class RecursiveFibonacci to include javadoc

--- RecursiveFibonacci.java
+++ RecursiveFibonacci.java
@@ -9,7 +9,12 @@ public class RecursiveFibonacci {
 
         System.out.println("The nth term of the Fibonacci sequence is: " + nthFibonacci(n));
     }
-
+    /**
+     * Static method that accepts an integer and returns the nth term in the Fibonacci Sequence
+     *
+     * @param n an integer provided by the user from main method
+     * @return the term at the specified location in the Fibonacci Sequence
+     */
     static int nthFibonacci(int n) {
         if (n <= 1) {
             return n;
